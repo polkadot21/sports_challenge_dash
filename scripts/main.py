7ce1257ed1ffecb9173b64d011ad278623f40f68
Evgenii Saurov <saurov.ev@gmail.com>
2022-01-08 14:13:29 +0100

    corrected x-axis to date

--- scripts/main.py
+++ scripts/main.py
@@ -9,6 +9,7 @@ def load_data(path):
     df = pd.read_excel(path)
 
     columns = [
+        'Date',
         'Sum_1',
         'Sum_2',
         'Sum_3',
@@ -18,22 +19,24 @@ def load_data(path):
         'Sum_7']
 
     df = df[columns]
+    df.index = pd.to_datetime(df['Date'])
     return df
 
 
 def plot_bar_chart(df):
     traces = []
+    teams = df.columns.to_list()[1:]
     fig = go.Figure()
     fig.update_layout(width=2100,
                                height=700,
                                template=pio.templates['plotly_dark'],
                                title="Daily results of Event 1")
 
-    for team in df.columns.to_list():
+    for team in teams:
         trace = go.Bar(x=df.index,
                              y=df[team],  # temperature
                              showlegend=True,
-                             name = 'Team {}'.format(df.columns.to_list().index(team)+1)
+                             name = 'Team {}'.format(teams.index(team)+1)
                              )
         traces.append(trace)
     fig.add_traces(traces)
@@ -44,10 +47,10 @@ def plot_bar_chart(df):
 
 
 def plot_cum_chart(df):
-    df_ = df.copy()
-
+    teams = df.columns.to_list()[1:]
+    df_ = df[teams]
     #cumulative kms
-    for i in range(0, len(df)-1):
+    for i in range(0, len(df_)-1):
         df_.iloc[i+1] = df_.iloc[i] + df_.iloc[i+1]
 
     traces = []
@@ -57,11 +60,11 @@ def plot_cum_chart(df):
                                template=pio.templates['plotly_dark'],
                                title="Overall results after Event 1")
 
-    for team in df.columns.to_list():
+    for team in teams:
         trace = go.Scattergl(x=df_.index,
                              y=df_[team],  # temperature
                              showlegend=True,
-                             name='Team {}'.format(df.columns.to_list().index(team) + 1)
+                             name='Team {}'.format(teams.index(team) + 1)
                              )
         traces.append(trace)
     fig.add_traces(traces)
